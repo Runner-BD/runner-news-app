cee45264348d1de24988b616e8d0882604bf3b0c
Runner-BD <dcsale.dg@gmail.com>
2026-02-28 17:22:30 +0600

    Update app.py

--- app.py
+++ app.py
@@ -280,5 +280,7 @@ def fetch_news():
 # MAIN
 # -----------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
